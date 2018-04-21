4a86aac17ead4d32ae7cb84742be3669150df0ff
taha falahati <taha.fala@yahoo.com>
2018-04-22 02:08:57 +0430

    simple subtitle

--- User/main.c
+++ User/main.c
@@ -1,5 +1,6 @@
 #include "stm32f0xx.h"
 #include "TF_LCD.h"
+#include <stdio.h>
 
 uint32_t micros;
 
@@ -8,8 +9,10 @@ void delayMs(uint32_t ms);
 void delayUs(uint32_t us);
 void gpio_init(void);
 
-const uint8_t rows = 4;
-const uint8_t cols = 20;
+
+const uint8_t numRows = 4;
+const uint8_t numCols = 20;
+char subtitle[] = "The quick brown fox jumps over the lazy dog :)";
 
 int main(){
 	micros=0;
@@ -20,10 +23,19 @@ int main(){
 	TF_lcdDisplayControl();
 	TF_lcdClear();
 	TF_lcdGotoXY(1,1);
-	TF_lcdPrint((uint8_t*) "I'm Taha falahati from thetick");
-	TF_lcdClear();
+	//TF_lcdPrint((uint8_t*) "I'm Taha falahati from thetick");
 	while(1){
-		
+		for(int pos=0; pos<sizeof(subtitle)+(numCols); pos++){
+			for(int x=1; x<=numCols; x++){
+				TF_lcdGotoXY(4,x);
+				char* tmp = subtitle + pos + x -1 -numCols;
+				if((tmp<subtitle) || (tmp>(subtitle+sizeof(subtitle)-2))) // || tmp>subtitle+sizeof(subtitle)
+					TF_lcdPrintCh(' ');
+				else
+					TF_lcdPrintCh(*(tmp));
+			}
+			delayMs(160);
+		}
 	}
 }
 
